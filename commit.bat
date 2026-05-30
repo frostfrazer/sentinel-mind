13dfc68166898b1c69fa3ac10b8bebfb0cc5ef62
SentinelMind <dev@sentinelmind.com>
2026-05-30 13:54:49 +0300

    Point dashboard at live Render API

---
+++ commit.bat
@@ -0,0 +1,3 @@
+cd /d C:\Users\DNjeri\Documents\sentinelmind
+git commit -m "Initial commit"
+echo COMMITTED
---
+++ push.bat
@@ -0,0 +1,5 @@
+cd /d C:\Users\DNjeri\Documents\sentinelmind
+git add -A
+git commit -m "Point dashboard at live Render API"
+git push origin main
+echo PUSHED
